cb448f2e023bc4930fd7f71f29629da00597d1c6
zahrabateninia <zahrabatenin@gmail.com>
2024-04-08 18:47:48 +0330

    Make the given array unique( no duplicated items) and sorted

--- bst.js
+++ bst.js
@@ -13,5 +13,12 @@ class Tree{
         this.root = this.buildTree(array)
     }
 
-}
+    // The buildTree function should return the level-0 root node.
+    buildTree(array){
+        // remove duplicates and sort array
+        let uniqueArray = Array.from(new Set(array));
+        let uniqueSortedArray = uniqueArray.sort((a, b) => a - b)
+        return uniqueSortedArray
+    }
 
+}
