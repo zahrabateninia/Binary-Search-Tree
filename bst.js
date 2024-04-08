d58610b0a10f91694dd2098680bb9c55ece57b3e
zahrabateninia <zahrabatenin@gmail.com>
2024-04-08 18:53:18 +0330

    Write the base case of constructBST method

--- bst.js
+++ bst.js
@@ -18,7 +18,19 @@ class Tree{
         // remove duplicates and sort array
         let uniqueArray = Array.from(new Set(array));
         let uniqueSortedArray = uniqueArray.sort((a, b) => a - b)
-        return uniqueSortedArray
+        return this.constructBST(uniqueSortedArray, 0, uniqueSortedArray.length -1)
+        
+    }
+
+    constructBST(array, start, end){
+        // base case
+        if(start>end){
+            return null;
+        }
+
+        // get the middle element of the array and make it root
+        
+
     }
 
 }
