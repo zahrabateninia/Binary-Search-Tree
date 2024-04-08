525f07a57116ed2a6cd1e2893faa23c5641b8b68
zahrabateninia <zahrabatenin@gmail.com>
2024-04-08 22:57:10 +0330

    Add prettyPrint function to console.log the tree in the structured format

--- bst.js
+++ bst.js
@@ -37,8 +37,20 @@ class Tree{
         array.right = this.constructBST(array, mid+1, end);
 
         return rootNode;
-
-
     }
+
+    prettyPrint = (node, prefix = "", isLeft = true) => {
+        if (node === null) {
+          return;
+        }
+        if (node.right !== null) {
+          prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+          prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+      };
+     
 
 }
